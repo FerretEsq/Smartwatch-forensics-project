0f6faf97353c1e5347ce3a0c7dd76267841e7496
Tom Tzivian <TomTziv@gmail.com>
2021-03-29 14:34:27 +0100

    sort script update

--- sort.py
+++ sort.py
@@ -1,4 +1,26 @@
 #Meme machine
-import re, fnmatch, datetime
 
-print(datetime.datetime.now())
+
+
+def main():
+    import fnmatch, datetime, os
+    path = os.getcwd()
+    for item in os.scandir(path):
+        if os.path.isdir(item):
+            if item.name == ".git":
+                pass
+            else:
+                print("__")
+                print("  |"+item.name)# +" is a directory")
+                #print("Traversing "+path+"/"+item.name+":")
+                for item in os.scandir(path+"/"+item.name):
+                    print("  |--"+item.name)
+                    if os.path.isdir(item):
+                        print("  |__"+"\n     |"+item.name) # is a directory")
+                        for thing in os.scandir(item):
+                            print("     |--"+thing.name)
+                    
+
+
+if __name__ == "__main__":
+    main()
